bc144b0c1b1f8d5eaa5d560fde684823ad8ebfcb
Mayuri Nimonkar <mayuri.nimonkar@aventior.com>
2025-01-01 11:42:38 +0530

    #memorygame: flip if no match and show score and result

--- memorygame/app.js
+++ memorygame/app.js
@@ -52,9 +52,11 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector("#grid");
+const result = document.querySelector("#result");
 let cardsChosen = [];
 let cardsChosenIds = [];
 const cardsWon = [];
+
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement("img");
@@ -73,19 +75,29 @@ function checkMatch() {
   const cards = document.querySelectorAll("#grid img");
 
   if (optionOneId == optionTwoId) {
+    cards[optionOneId].setAttribute("src", "images/blank.png");
+    cards[optionTwoId].setAttribute("src", "images/blank.png");
     alert("You clicked the same card!");
-  }
-  if (cardsChosen[0] === cardsChosen[1]) {
+  } else if (cardsChosen[0] === cardsChosen[1]) {
     alert("You found a match!");
     cards[optionOneId].setAttribute("src", "images/white.png");
     cards[optionTwoId].setAttribute("src", "images/white.png");
     cards[optionOneId].removeEventListener("click", flipCard);
     cards[optionTwoId].removeEventListener("click", flipCard);
     cardsWon.push(cardsChosen);
+    console.log(cardsWon);
+  } else {
+    cards[optionOneId].setAttribute("src", "images/blank.png");
+    cards[optionTwoId].setAttribute("src", "images/blank.png");
+    alert("Sorry try again!");
   }
-
+  result.textContent = cardsWon.length;
   cardsChosen = [];
   cardsChosenIds = [];
+
+  if (cardsWon.length === cardArray.length / 2) {
+    result.innerHTML = "Congratulations, you found them all!";
+  }
 }
 
 function flipCard() {
@@ -93,9 +105,10 @@ function flipCard() {
 
   cardsChosen.push(cardArray[cardId].name);
   cardsChosenIds.push(cardId);
+
   this.setAttribute("src", cardArray[cardId].img);
 
-  if (cardsChosen === 2) {
+  if (cardsChosen.length === 2) {
     setTimeout(checkMatch, 500);
   }
 }
