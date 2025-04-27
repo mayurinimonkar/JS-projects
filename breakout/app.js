543abe14854d46c12af3729ac4e10f89f287d3fc
Mayuri Nimonkar <mayuri.nimonkar@aventior.com>
2025-04-27 20:11:54 +0530

    #breakout: add user

--- breakout/app.js
+++ breakout/app.js
@@ -2,6 +2,9 @@ const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
 
+const userStart = [230, 10];
+let currentPosition = userStart;
+
 class Block {
   constructor(xAxis, yAxis) {
     this.bottomLeft = [xAxis, yAxis];
@@ -43,3 +46,9 @@ function addBlocks() {
 }
 
 addBlocks();
+
+const user = document.createElement("div");
+user.classList.add("user");
+user.style.left = currentPosition[0] + "px";
+user.style.bottom = currentPosition[1] + "px";
+grid.appendChild(user);
